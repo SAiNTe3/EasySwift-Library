178dec66dcfe3bcf8faf60a1876f7a8262c8e5ad
SAiNTe3 <3441486229@qq.com>
2025-03-26 22:55:02 +0800

    Update v1.1.3 Fixed

--- Demo.cpp
+++ Demo.cpp
@@ -1,32 +1,15 @@
 #include "include/Window.hpp"
-#include "include/ImGUI.hpp"
+
 int main()
 {
 	Window::Initialize(4, 6);
 	Window window(800, 450, "DemoWindow", true, true);
-	imgui_init(window.getWindow());
 	while (window.isOpen())
 	{
-		window.pollEvents();
-		ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        // 创建 ImGui 窗口
-        ImGui::Begin("Hello, ImGui!");
-        ImGui::Text("This is a simple ImGui window.");
-        if (ImGui::Button("Close"))
-            window.closeWindow();
-        ImGui::End();
-
-        // 渲染
-        ImGui::Render();
+		Event e;
+		window.pollEvents(e);
 		window.clear();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		window.display();
 	}
-	ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
 	Window::Terminate();
 }
--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 cd src
-g++ -c Audio.cpp AudioEngine.cpp Shader.cpp Sprite.cpp Texture.cpp Keyboard.cpp Mouse.cpp Window.cpp Clock.cpp Cursor.cpp -I../include -I../include/glad -I../include/stbImage -I../include/miniAudio -I../include/glm
+g++ -c Audio.cpp AudioEngine.cpp Shader.cpp Sprite.cpp Texture.cpp Keyboard.cpp Mouse.cpp Window.cpp Clock.cpp Cursor.cpp Event.cpp -I../include -I../include/glad -I../include/stbImage -I../include/miniAudio -I../include/glm
 cd ..
 ar rcs lib/multimedia.lib src/*.o
 del src\*.o
